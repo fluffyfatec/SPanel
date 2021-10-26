43107ca91e4ea9e9d1f2d1e897f8b37b5930ff0e
Jordan Lima <60404264+Jordan-Lima@users.noreply.github.com>
2021-10-25 23:09:54 -0300

    Bot de automatizar
    O bot tem como função automatizar o download da base de dados

---
+++ Sprint_4/automatizar.py
@@ -0,0 +1,27 @@
+import asyncio # pip install asyncio
+from playwright.async_api import async_playwright # pip install --upgrade pip, pip install playwright, playwright install
+
+# Link do site
+site_covid = 'https://www.saopaulo.sp.gov.br/planosp/simi/dados-abertos/'
+clicar_botao = '//html/body/section[4]/div/div/article[1]/div/ul/li[1]/div/a/span[1]'
+
+# PLaywright para acesso do CSV
+async def main():
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
+        page = await browser.new_page(accept_downloads = True)
+        await page.goto(site_covid)
+        await page.click('h3')
+        await page.click(clicar_botao)
+        
+        # Baixar arquivo
+        async with page.expect_download() as download_info:
+            download = await download_info.value
+            path = await download.path()
+            await download.save_as('./docs/Database_covid19.csv')
+            print(download.url, path)
+            await page.close()
+            await browser.close()
+
+asyncio.run(main())
+
