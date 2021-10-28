4ef30280f63c50c4554a3d536ad129e781d2e640
Patrick Souza <patrickgabriel103f@gmail.com>
2021-10-28 15:15:14 -0300

    Integração do BOT com os CSV's

--- Sprint_4/bot.py
+++ Sprint_4/bot.py
@@ -1,14 +1,31 @@
+#------Bibliotecas------#
 import telebot
 import pandas as pd
-# t.me/fluffyapi_bot   caminho para o bot
-CHAVE_API = "2054963815:AAF-v9jj4Jw8gDiwEXC7OZAMXXn5fy1LZjA"
+#-----------------------#
+
+# t.me/Fluffyapi_bot   caminho para o bot
+CHAVE_API = "2068198957:AAFKYaIzYWW8pYmhB1_V5-TorX3pfp1xn7A"
+
+df = pd.read_csv("docs\df_estadotratado.csv")
+
+date_column = df["datahora"]
+max_value = date_column.max()
+row = df.loc[df["datahora"] == max_value]
+
+# Dados mais atualizados Covid19
+lastDate = row["datahora"].values[0]
+cases = row["casos"].values[0]
+new_cases = row["casos_novos"].values[0]
+deaths = row["obitos"].values[0]
+new_deaths = row["obitos_novos"].values[0]
+population = row["pop"].values[0]
 
 bot = telebot.TeleBot(CHAVE_API)
 
 #obitos
 @bot.message_handler(commands=["obitos"])
 def obitos(mensagem):
-    bot.send_message(mensagem.chat.id, "Ops ainda não temos dados")
+    bot.send_message(mensagem.chat.id, f"Dados do dia:  {lastDate}\n  \nÓbitos Totais:  {deaths}\n \nNovos óbitos:  {new_deaths}. ")
 
 #imunizados
 @bot.message_handler(commands=["imunizados"])
@@ -16,17 +33,23 @@ def imunizados(mensagem):
     bot.send_message(mensagem.chat.id, "Ops ainda não temos dados")
 
 #novos casos no periodo
-@bot.message_handler(commands=["novoscasos"])
-def novoscasos(mensagem):
-    bot.send_message(mensagem.chat.id, "Ops ainda não temos dados")
+@bot.message_handler(commands=["casos"])
+def casos(mensagem):
+    bot.send_message(mensagem.chat.id, f"Dados do dia:  {lastDate}\n  \nCasos Totais:  {cases}\n \nNovos casos: {new_cases}")
+
+#população do estado de SP
+@bot.message_handler(commands=["pop"])
+def casos(mensagem):
+    bot.send_message(mensagem.chat.id, f" Atualmente a população do estado de São Paulo é de {population} pessoas.")
+
 
 @bot.message_handler(commands=["dados"])
 def opcao1(mensagem):
     texto =  """ 
     O que você quer vizualizar? (Clique em uma opção)
-    /obitos Obitos
+    /obitos Óbitos
     /imunizados Imunizados
-    /novoscasos Novos Casos"""
+    /casos Casos"""
     bot.send_message(mensagem.chat.id, texto)
 
 
@@ -49,6 +72,7 @@ def responder(mensagem):
      Escolha uma opção para continuar (clique no item)
     
     /dados Vizualizar dados da Covid19
+    /pop População do estado de São Paulo
     /sobre Sobre
     
     Qualquer outra opção não vai funcionar"""
