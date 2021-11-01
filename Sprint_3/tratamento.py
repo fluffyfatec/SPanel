d1b1e8a9f86cb94547859cd8221e5beda25bb479
Fluffy Integrative Project - FATEC <88806221+fluffyfatec@users.noreply.github.com>
2021-11-01 17:50:15 -0300

    pop vacinados att

--- Sprint_3/tratamento.py
+++ Sprint_3/tratamento.py
@@ -8,12 +8,12 @@ df_state = df_state.drop(
     columns=['dia', 'mes', 'casos_pc', 'casos_mm7d', 'obitos_pc', 'obitos_mm7d', 'letalidade', 'nome_ra'
          , 'cod_ra', 'nome_drs', 'cod_drs', "pop_60", 'area', 'map_leg', 'map_leg_s', 'latitude', 'longitude','semana_epidem','codigo_ibge'])
 df_state['nome_munic'] = df_state['nome_munic'].str.upper()
+df_state['datahora'] = pd.to_datetime(df_state['datahora'],format='%d/%m/%Y')
 
 # Tratando dados estado
 df_estadotratado = df_state.drop(
     columns=['nome_munic'])
 df_estadotratado = df_estadotratado.assign(estado='SÃO PAULO')
-df_estadotratado['datahora'] = pd.to_datetime(df_estadotratado['datahora'],format='%d/%m/%Y')
 df_estadotratado.sort_values("datahora")
 df_estadotratado['casos'] = df_estadotratado.groupby('datahora')["casos"].transform(sum)
 df_estadotratado['casos_novos'] = df_estadotratado.groupby('datahora')["casos_novos"].transform(sum)
@@ -50,15 +50,19 @@ df_terceira = df_terceira.rename(
     columns={'Total Doses Aplicadas':'terceiradose'})
 df_terceira = df_terceira.drop(
     columns=['Dose'])
+df_pop = df_state.drop(
+    columns=['datahora', 'casos','casos_novos','obitos','obitos_novos'])
+df_pop=df_pop.drop_duplicates(subset='nome_munic', keep='first')
 
 # Junção das doses no df_vacinas
 df_vacinastratado = pd.merge(df_unica,df_primeira, how='inner', on='nome_munic')
 df_vacinastratado = pd.merge(df_vacinastratado,df_segunda, how='inner', on='nome_munic')
 df_vacinastratado = pd.merge(df_vacinastratado,df_terceira, how='inner', on='nome_munic')
+df_vacinastratado = pd.merge(df_vacinastratado,df_pop, how='inner', on='nome_munic')
 df_vacinastratado.fillna(('-'), inplace=True)
 
+
 # Junção das vacinas.csv e df_state.csv no df_tratado.csv
-df_state['datahora'] = pd.to_datetime(df_state['datahora'],format='%d/%m/%Y')
 df_state.sort_values("datahora")
 df_state.to_csv("docs/df_tratado.csv")
 df_vacinastratado.to_csv("docs/df_vacinastratado.csv")
